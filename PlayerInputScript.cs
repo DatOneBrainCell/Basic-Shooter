878fd8ae55da832bd83be6233b0bccb1a0b44958
DatOneBrainCell <crjasperdaniel@gmail.com>
2026-04-16 21:26:09 +0530

    Create PlayerInputScript.cs

---
+++ PlayerInputScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputScript : MonoBehaviour
+{
+    private const string MOUSE_X = "Mouse X";
+    private const string MOUSE_Y = "Mouse Y";
+
+
+    public float GetMouseXAxis() {
+
+        return Input.GetAxisRaw(MOUSE_X);
+    }
+
+    public float GetMouseYAxis() {
+
+        return Input.GetAxisRaw(MOUSE_Y);
+    }
+}
